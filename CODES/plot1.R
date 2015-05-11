e6791cd5268afe72ef2eb94e39a0d97f05cda292
LIU YITONG <pkulyt@users.noreply.github.com>
2015-05-11 10:08:24 +0800

    Update plot1.R

--- CODES/plot1.R
+++ CODES/plot1.R
@@ -1,1 +1,3 @@
-> getws()
+> getwd()
+[1] "/Users/apple/Desktop/coursera/exploratory data analysis/WEEK1"
+data<-read.table(text = grep("^[1,2]/2/2007", readLines("household_power_consumption.txt"), value = TRUE), col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", header = TRUE)
